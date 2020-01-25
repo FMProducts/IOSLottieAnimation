da77859031c572a75ebfb82f0faebcff8f03ac58
FarizMamedow <fariz.mamedow22@gmail.com>
2020-01-25 15:46:25 +0500

    start

---
+++ IOSLottieAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSLottieAnimation
+//
+//  Created by ideal on 1/25/20.
+//  Copyright © 2020 fmproduct.lottie.animation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
