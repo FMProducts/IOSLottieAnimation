d7a290ce1eccf9497f1ce1be0edf3797511ceb65
FarizMamedow <fariz.mamedow22@gmail.com>
2020-01-25 17:44:28 +0500

    finish

--- IOSLottieAnimation/ViewController.swift
+++ IOSLottieAnimation/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
-
+    
+    let animationView = AnimationView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let animation = Animation.named("animation")
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.frame.size = CGSize(width: UIScreen.main.bounds.width / 2, height: UIScreen.main.bounds.height / 2)
+        animationView.center = self.view.center
+        self.view.addSubview(animationView)
     }
 
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.animationView.play(fromProgress: 0,
+                           toProgress: 1,
+                           loopMode: LottieLoopMode.loop,
+                           completion: { (finished) in
+                            if finished {
+                              print("Animation Complete")
+                            } else {
+                              print("Animation cancelled")
+                            }
+                        })
+    }
 
 }
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'IOSLottieAnimation' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for IOSLottieAnimation
+  pod 'lottie-ios'
+
+end
